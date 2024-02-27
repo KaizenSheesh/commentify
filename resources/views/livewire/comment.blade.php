e1fa445d291d543d27ec71a0811ed25b0444accd
Kaizen <119792622+KaizenSheesh@users.noreply.github.com>
2024-02-27 21:33:57 +0700

    update: ui

--- resources/views/livewire/comment.blade.php
+++ resources/views/livewire/comment.blade.php
@@ -23,8 +23,14 @@
                 </div>
                 <div class="relative">
                     <button wire:click="$toggle('showOptions')"
-                        class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-blue-700 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                        type="button">Upload
+                        class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        type="button">
+                        <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z">
+                            </path>
+                        </svg>
                     </button>
                     <!-- Dropdown menu -->
                     @if ($showOptions)
